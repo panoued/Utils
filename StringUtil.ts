fe6c060d6a415b3326ae20fc515dc55e82cd9e29
panoued <panoued@163.com>
2020-06-18 10:24:27 +0800

    update

--- StringUtil.ts
+++ StringUtil.ts
@@ -4,4 +4,35 @@ export default class StringUtil {
         return !str || str.split(" ").join("").length === 0 || str.split("\n").join("").length === 0;
     };
 
+    static toString(value: any){
+        let res = '';
+        if(value){
+            if(typeof value == 'number'){
+                res = String(value);
+            }else if(typeof value == 'string'){
+                res = value;
+            };
+        }else if(value === 0){
+            res = '0';
+        };
+        return res;
+    };
+
+    static getFormatFileSizeString(fileSize: number, withB = false): string{
+        let result: string = '';
+        const b = withB ? 'B' : '';
+        if(fileSize < 1024){
+            result = fileSize + b;
+        }else if(fileSize < Math.pow(1024, 2)){
+            result = Math.round(fileSize / 1024) + 'K' + b;
+        }else if(fileSize < Math.pow(1024, 3)){
+            result = Math.round(fileSize / Math.pow(1024, 2)) + 'M' + b;
+        }else if(fileSize < Math.pow(1024, 4)){
+            result = Math.round(fileSize / Math.pow(1024, 3)) + 'G' + b;
+        }else{
+            result = Math.round(fileSize / Math.pow(1024, 4)) + 'T' + b;
+        };
+        return result;
+    };
+
 }
